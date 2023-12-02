be655a86bcdca73b89e29d866a92099edd71b0d2
Jan Helge Aalbu <jhaalbu@gmail.com>
2023-12-02 08:26:17 +0100

    fjerna django secret key

--- varslingsdata/varslingsdata/settings.py
+++ varslingsdata/varslingsdata/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
+from dotenv import load_dotenv
 
 from pathlib import Path
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y0(#)7-ub^svs@%%l0j$^sfh@s5v454#wr)(c6=5rn)bkpr1_a"
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key-for-local')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
